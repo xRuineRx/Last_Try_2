28a841fdadf64876f82f21896f2bb891553675be
xRuineRx <115977690+xRuineRx@users.noreply.github.com>
2024-05-23 18:55:15 +0300

    Delete RPG_GAMEDESK directory

--- RPG_GAMEDESK/RPG_FUNCTIONS/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from .views import GamePostList, GamePostCreate
-
-# Импортируем созданное нами представление
-
-urlpatterns = [
-    path('All_Posts', GamePostList.as_view()),
-    path('All_Posts/add_post', GamePostCreate.as_view()),
-]
